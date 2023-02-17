528737362963b5f5e26162717d7e0c11b026587c
aaron-wilkowitz <72474472+aaron-wilkowitz@users.noreply.github.com>
2023-02-17 15:13:58 -0500

    Create 4_call_procedure.sql

---
+++ 4_call_procedure.sql
@@ -0,0 +1,5 @@
+CALL `deid_demo_input.deidentify_table` ('deid_demo_input.deidentify_input_variables');
+
+# put differently: CALL [your_dataset.your_procedure] ('[your_metadata_table]')
+# replace your procedure name right after CALL 
+# replace your metadata table inside the parentheses
